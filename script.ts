018ca7a4c86429dc97fa714f67dc1d2e8757eecc
Gabriel Sbruzzi <132472378+GabrielSbruzzi@users.noreply.github.com>
2024-12-26 08:06:52 -0300

    Delete script.ts

--- script.ts
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    alert(`Você clicou no botão: ${button.textContent}`);
-  });
-});
